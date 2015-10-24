b92b5c0bcec3c96e1c99d2e0a0a13d5edd6ad9f3
Matthew <Kinematic12@gmail.com>
2015-10-24 16:02:25 +0200

    Initial Commit

---
+++ Beat the TrafficTests/Beat_the_TrafficTests.swift
@@ -0,0 +1,36 @@
+//
+//  Beat_the_TrafficTests.swift
+//  Beat the TrafficTests
+//
+//  Created by Matthew Curtner on 10/24/15.
+//  Copyright © 2015 Matthew Curtner. All rights reserved.
+//
+
+import XCTest
+@testable import Beat_the_Traffic
+
+class Beat_the_TrafficTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
