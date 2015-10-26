9c7d1413d3be35c1dd8cdaba2da36b6b7ef25d4a
Matthew <Kinematic12@gmail.com>
2015-10-26 17:03:50 +0100

    Adding Physics

--- Beat the Traffic/GameScene.swift
+++ Beat the Traffic/GameScene.swift
@@ -12,40 +12,79 @@ enum Layers: CGFloat {
     case Background = -1
     case Road
     case RoadLines
+    case Coins
     case Cars
+    case PlayerCar
 }
 
-class GameScene: SKScene {
+struct PhysicsCategory {
+    static let Coin: UInt32 = 1
+    static let Car: UInt32 = 2
+    static let PlayerCar: UInt32 = 3
+}
+
+
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
+    // Cars
     let car = SKSpriteNode(imageNamed: "Audi")
     var car1 = SKSpriteNode(imageNamed: "Car")
     var car2 = SKSpriteNode(imageNamed: "Car")
     var car3 = SKSpriteNode(imageNamed: "Car")
     var car4 = SKSpriteNode(imageNamed: "Car")
     
+    // Speed at which cars are moving down
     var kDownwardSpeed: CGFloat = 3.0
     
+    // Cars
     let vehicleImageArray = ["Mini_truck","Car", "taxi"]
     var carSpriteArray: Array<SKSpriteNode> = []
     
+    // Coin Textures
+    var textureAtlas = SKTextureAtlas()
+    var textureArray = [SKTexture]()
+    var mainCoin = SKSpriteNode()
     
+
     override func didMoveToView(view: SKView) {
         
-        carSpriteArray.append(car1)
-        carSpriteArray.append(car2)
-        carSpriteArray.append(car3)
-        carSpriteArray.append(car4)
+        // We implement SKPhysicsContactDelegate to get called back when a contact occurs
+        // Register ourself as the delegate
+        self.physicsWorld.contactDelegate = self;
         
+        // Add the coin images from the texture atlas
+        addImagesToArrayFromTextureAtlas()
+//
+//        // Setup cars
+//        setupCarsArray()
+//        
+//        // Create and add the UI Elements
         createBackground()
         createRoad()
         createPlayerCar()
+//
+//        // Add the total number of cars to the View
+////        addCarsToStreet(car1)
+////        addCarsToStreet(car2)
+////        addCarsToStreet(car3)
+////        addCarsToStreet(car4)
         
-        addCarsToStreet(car1)
-        addCarsToStreet(car2)
-        addCarsToStreet(car3)
-        addCarsToStreet(car4)
-        
+        // Create the coins
+        createCoins()
+        createCoins()
+    
     }
+    
+    
+    func didBeginContact(contact: SKPhysicsContact) {
+        print("Hello")
+    }
+    
+    func collisionWithCoin(PlayerCar PlayerCar:SKSpriteNode, Coin: SKSpriteNode) {
+        mainCoin.removeFromParent()
+    }
+    
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
@@ -53,7 +92,7 @@ class GameScene: SKScene {
         for touch in touches {
             let location = touch.locationInNode(self)
             
-            // Not allowing the ship to pass a certain height
+            // Not allowing the player car to pass the width of the street
             if location.x < frame.size.width/2 {
                 car.runAction(SKAction.moveToX(frame.size.width/2 - 32, duration: 0.5))
             }
@@ -66,8 +105,9 @@ class GameScene: SKScene {
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        
+        
         for car in carSpriteArray {
-            
             // Downward speed of cars
             car.position.y -= kDownwardSpeed
             
@@ -77,6 +117,13 @@ class GameScene: SKScene {
                 addCarsToStreet(car)
             }
         }
+        
+        mainCoin.position.y -= kDownwardSpeed
+        if mainCoin.position.y < -frame.size.height {
+            mainCoin.removeFromParent()
+            createCoins()
+        }
+        
     }
     
     func createBackground() {
@@ -101,31 +148,98 @@ class GameScene: SKScene {
     func createPlayerCar() {
         car.size = CGSizeMake(128.0, 128.0)
         car.position = CGPoint(x:frame.size.width/2 + 33, y: frame.size.height/2)
-        car.zPosition = Layers.Cars.rawValue
+        car.zPosition = Layers.PlayerCar.rawValue
         car.name = "playerCar"
-        addChild(car)
+        
+        car.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: 35.0, height: 110))
+        car.physicsBody!.affectedByGravity = false
+        car.physicsBody!.categoryBitMask = PhysicsCategory.PlayerCar
+        car.physicsBody!.contactTestBitMask = PhysicsCategory.Coin
+        car.physicsBody!.dynamic = false
+
+        
+        self.addChild(car)
     }
     
+    
+    func addImagesToArrayFromTextureAtlas() {
+        textureAtlas = SKTextureAtlas(named: "Coin")
+        
+        // Sort Array
+        for i in 1...textureAtlas.textureNames.count {
+            let name = "coin\(i).png"
+            textureArray.append(SKTexture(imageNamed: name))
+        }
+    }
+    
+    func createCoins() {
+        let randomX = getRandomness(zeroToValue: 2)
+        let randomY = getRandomness(zeroToValue: frame.size.height * 2)
+        
+        mainCoin = SKSpriteNode(imageNamed: textureAtlas.textureNames[0])
+        mainCoin.size = CGSize(width: 50, height: 50)
+        
+        if randomX == 0 {
+            mainCoin.position = CGPoint(x: frame.size.width/2 - 32, y: randomY + frame.size.height)
+        }
+        if randomX == 1 {
+            mainCoin.position = CGPoint(x: frame.size.width/2 + 32, y: randomY + frame.size.height)
+        }
+        mainCoin.zPosition = Layers.Coins.rawValue
+        mainCoin.name = "coins"
+        
+        // Coin Physics
+        mainCoin.physicsBody = SKPhysicsBody(rectangleOfSize: mainCoin.frame.size)
+        mainCoin.physicsBody!.affectedByGravity = false
+        mainCoin.physicsBody!.categoryBitMask = PhysicsCategory.Coin
+        mainCoin.physicsBody!.contactTestBitMask = PhysicsCategory.PlayerCar
+        mainCoin.physicsBody!.dynamic = true
+        addChild(mainCoin)
+        
+        //mainCoin.runAction(SKAction.repeatActionForever(SKAction.animateWithTextures(textureArray, timePerFrame: 0.09)))
+    }
+    
+
+    func setupCarsArray() {
+        carSpriteArray.append(car1)
+        carSpriteArray.append(car2)
+        carSpriteArray.append(car3)
+        carSpriteArray.append(car4)
+    }
+    
+    
     func addCarsToStreet(carSprite: SKSpriteNode) {
-        let randomX = Int(arc4random_uniform(UInt32(2)))
-        let randomY = CGFloat(arc4random_uniform(UInt32(frame.size.height * 2)))
-        let randomNum = Int(arc4random_uniform(UInt32(2)))
+        let randomX = getRandomness(zeroToValue: 2)
+        let randomY = getRandomness(zeroToValue: frame.size.height * 2)
+        let randomNum = Int(getRandomness(zeroToValue: 2))
         
         carSprite.texture = SKTexture(imageNamed: vehicleImageArray[randomNum])
         carSprite.size = CGSizeMake(128.0, 128.0)
+        carSprite.zPosition = Layers.Cars.rawValue
         
+        carSprite.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: 35.0, height: 110))
+        //carSprite.physicsBody?.categoryBitMask = PhysicsCategory.Car
+        //carSprite.physicsBody?.contactTestBitMask = PhysicsCategory.PlayerCar
+        carSprite.physicsBody?.affectedByGravity = false
+        carSprite.physicsBody?.dynamic = false
         
         // If randomX = 0, place car on the left side
         if randomX == 0 {
-            carSprite.position = CGPoint(x:frame.size.width/2 - 33, y: randomY + carSprite.size.height)
+            carSprite.position = CGPoint(x:frame.size.width/2 - 33, y: randomY + carSprite.size.height + frame.size.height)
         }
         // If randomX = 1, place car on the right side
         if randomX == 1 {
-            carSprite.position = CGPoint(x:frame.size.width/2 + 33, y: randomY + carSprite.size.height)
+            carSprite.position = CGPoint(x:frame.size.width/2 + 33, y: randomY + carSprite.size.height + frame.size.height)
         }
         
         carSprite.name = "regularCars"
         addChild(carSprite)
+    }
+    
+    
+    // Return the random value between 0 and the provided parameter
+    func getRandomness(zeroToValue zeroToValue: CGFloat) -> CGFloat {
+        return CGFloat(arc4random_uniform(UInt32(zeroToValue)))
     }
 }
 
